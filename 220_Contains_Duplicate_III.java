091cd6dfeb8615592b210ff1df4c0363b42a4811
Abhishek Uniyal <138280438+abhishek-uniyal01@users.noreply.github.com>
2024-06-23 20:43:51 +0530

    Create 220_Contains_Duplicate_III.java

---
+++ 220_Contains_Duplicate_III.java
@@ -0,0 +1,27 @@
+import java.util.TreeSet;
+
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        if (nums.length == 0 || indexDiff <= 0) {
+            return false;
+        }
+        
+        TreeSet<Long> set = new TreeSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            Long num = (long) nums[i];
+            
+            Long floor = set.floor(num + valueDiff);
+            Long ceiling = set.ceiling(num - valueDiff);
+            
+            if ((floor != null && floor >= num) || (ceiling != null && ceiling <= num)) {
+                return true;
+            }
+            
+            set.add(num);
+            if (i >= indexDiff) {
+                set.remove((long) nums[i - indexDiff]);
+            }
+        }
+        return false;
+    }
+}
