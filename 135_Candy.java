ebfd49f41ee9e5370be56cf60c7889b1b03c2ab1
Abhishek Uniyal <138280438+abhishek-uniyal01@users.noreply.github.com>
2024-06-26 15:11:18 +0530

    Create 135_Candy.java

---
+++ 135_Candy.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int arr[] = new int[ratings.length];
+        Arrays.fill(arr, 1);
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1])
+                arr[i] = arr[i - 1] + 1;
+        }
+        for (int i = ratings.length - 1; i > 0; i--) {
+            if (ratings[i] < ratings[i - 1] && arr[i] >= arr[i - 1]) {
+                arr[i - 1] = arr[i] + 1;
+            }
+        }
+        return Arrays.stream(arr).sum();
+    }
+}
