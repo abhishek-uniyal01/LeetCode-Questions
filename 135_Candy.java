667b81c9b84f0c3a54127d564ee850add8f0a126
Abhishek Uniyal <138280438+abhishek-uniyal01@users.noreply.github.com>
2024-07-29 19:20:17 +0530

    135

--- 135_Candy.java
+++ 135_Candy.java
@@ -2,6 +2,7 @@ class Solution {
     public int candy(int[] ratings) {
         int arr[] = new int[ratings.length];
         Arrays.fill(arr, 1);
+        
         for (int i = 1; i < ratings.length; i++) {
             if (ratings[i] > ratings[i - 1])
                 arr[i] = arr[i - 1] + 1;
